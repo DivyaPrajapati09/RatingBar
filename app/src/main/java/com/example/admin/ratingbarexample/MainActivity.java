50a1e9cda2f611e0bbf74c96d725e1a713cc84bd
DivyaPrajapati09 <divya4337@gmail.com>
2016-09-04 18:04:07 +0530

    initial commit

---
+++ app/src/main/java/com/example/admin/ratingbarexample/MainActivity.java
@@ -0,0 +1,25 @@
+package com.example.admin.ratingbarexample;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.RatingBar;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity implements RatingBar.OnRatingBarChangeListener {
+
+    RatingBar ratingBar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ratingBar = (RatingBar) findViewById(R.id.rating_bar);
+        ratingBar.setOnRatingBarChangeListener(this);
+    }
+
+    @Override
+    public void onRatingChanged(RatingBar ratingBar, float rating, boolean fromUser) {
+        Toast.makeText(MainActivity.this, "Rating value is : " + String.valueOf(rating), Toast.LENGTH_SHORT).show();
+    }
+}
